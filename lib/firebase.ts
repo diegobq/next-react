5ebca4e0ceae600d85ce486f8f170f32f37a67d7
diegobq <diegobq@gmail.com>
2025-04-25 20:19:06 -0300

    use public env var

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -2,13 +2,13 @@ import { getApp, getApps, initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDTqFxH0JlhBZaFk3aCOMPQ-IHAAD3eqN0',
-  authDomain: 'shareger-fa6fc.firebaseapp.com',
-  projectId: 'shareger-fa6fc',
-  storageBucket: 'shareger-fa6fc.firebasestorage.app',
-  messagingSenderId: '568891201542',
-  appId: '1:568891201542:web:eff592322e361972480b87',
-  measurementId: 'G-HK6T7WWSTD',
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
 }
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp()
