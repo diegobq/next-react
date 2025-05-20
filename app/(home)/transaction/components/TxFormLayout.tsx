ce7cc4a5b3d7a165e62b02b69e3b33fbacc86308
diegobq <diegobq@gmail.com>
2025-05-19 21:01:20 -0300

    justify start

--- app/(home)/transaction/components/TxFormLayout.tsx
+++ app/(home)/transaction/components/TxFormLayout.tsx
@@ -4,7 +4,7 @@ export default async function TxFormLayout({
   children: React.ReactNode
 }>) {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-50 dark:bg-gray-900 p-4">
+    <div className="min-h-screen flex items-start justify-center bg-gray-50 dark:bg-gray-900 p-4">
       <div className="w-full max-w-md space-y-4">{children}</div>
     </div>
   )
