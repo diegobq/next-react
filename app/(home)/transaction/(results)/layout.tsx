85edc7c934699341ac32606c4c7ca07a6ee8d74f
diegobq <diegobq@gmail.com>
2025-06-01 16:05:22 -0300

    remove tx title

--- app/(home)/transaction/(results)/layout.tsx
+++ app/(home)/transaction/(results)/layout.tsx
@@ -1,14 +1,10 @@
 export default async function ResultsLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode
-}>) {
+}) {
   return (
     <div className="min-h-screen bg-gray-50 dark:bg-gray-900 p-6 flex flex-col items-center">
-      <h1 className="text-3xl font-bold text-gray-900 dark:text-white mb-6">
-        Transactions
-      </h1>
-
       {children}
     </div>
   )
